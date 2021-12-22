b5e254ca355f064f2b559a318bf478dd414d6049
Korotenko Alex <alexey.korotenko90@gmail.com>
2021-12-22 11:20:46 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Container } from './components/Container';
+
+class Feeedback extends Component {
+  render() {
+    return <Container>null</Container>;
+  }
+}
+
+export default Feeedback;
